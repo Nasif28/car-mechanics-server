453ca3ecdd7abc959d3a9b3c13b86527cd7af288
Nasif Zeehan <nasifzeehan1@gmail.com>
2021-10-28 20:28:54 +0600

    port added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -62,10 +62,8 @@ async function run() {
 
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-    res.send('');
+    res.send('Running...');
 });
 
 app.listen(port, () => {
